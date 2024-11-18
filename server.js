de93ee78a5f993986a9e3e422ce8ef277fac598e
a.bacaj <a.bacaj@orangemelons.com>
2024-11-18 10:41:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+
+const app = express();
+
+// Load your OpenAPI YAML file
+const openapiDocumentation = yaml.load(path.join(__dirname, 'openapi.yaml'));
+
+// Set up Swagger UI with your documentation
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiDocumentation));
+
+app.listen(3000, () => {
+    console.log('API Docs available at http://localhost:3000/docs');
+});
